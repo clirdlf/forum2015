8cd30e653b1c62f2990627b65c34ca349e38fc5f
Wayne Graham <wgraham@clir.org>
2023-05-16 17:04:08 -0400

    Initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# DLF Forum 2014
+
+Archive of the 2014 DLF Forum
+
+## Development
+
+git clone ...
+npm install
+npm start
---
+++ eleventy.config.js
@@ -0,0 +1,46 @@
+const { DateTime } = require("luxon");
+const markdownItAnchor = require("markdown-it-anchor");
+
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginBundle = require("@11ty/eleventy-plugin-bundle");
+const pluginNavigation = require("@11ty/eleventy-navigation");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
+module.exports = function(eleventyConfig) {
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid"
+        ],
+
+        // Pre-process *.md files with: (default: `liquid`)
+		markdownTemplateEngine: "njk",
+
+		// Pre-process *.html files with: (default: `liquid`)
+		htmlTemplateEngine: "njk",
+
+        // These are all optional:
+		dir: {
+			input: "src",          // default: "."
+			includes: "_includes",  // default: "_includes"
+			layouts: "_layouts", 
+			data: "_data",          // default: "_data"
+			output: "_site"
+		},
+
+		// -----------------------------------------------------------------
+		// Optional items:
+		// -----------------------------------------------------------------
+
+		// If your site deploys to a subdirectory, change `pathPrefix`.
+		// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
+
+		// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
+		// it will transform any absolute URLs in your HTML to include this
+		// folder name and does **not** affect where things go in the output folder.
+		pathPrefix: "/",
+    };
+};
